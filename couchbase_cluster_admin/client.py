95ac1b9e479bba6d44861f0ada5bdb0de66691b2
Cosimo Streppone <cosimo@kahoot.com>
2023-04-12 18:10:26 +0200

    Added support for requests with JSON request body

--- couchbase_cluster_admin/client.py
+++ couchbase_cluster_admin/client.py
@@ -5,7 +5,7 @@ import requests
 
 
 class BaseClient:
-    def http_request(self, url, method="GET", data=None, headers={}, timeout=10.0):
+    def http_request(self, url, method="GET", data=None, json=None, headers={}, timeout=10.0):
         auth = None
         if self.username is not None and self.password is not None:
             auth = (self.username, self.password)
@@ -21,6 +21,7 @@ class BaseClient:
                     method,
                     url,
                     data=data,
+                    json=json,
                     headers=headers,
                     auth=auth,
                     timeout=timeout,
